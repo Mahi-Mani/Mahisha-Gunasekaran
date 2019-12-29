eba7295d28f0343eafb078de05a48c21194f07e2
Mahisha Gunasekaran <m.mahishamani@gmail.com>
2019-12-29 01:42:28 -0800

    Final Push

---
+++ assets/scripts/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    // Variable Declarations
+    var isExpandedAbt;
+    var isExpandedContact;
+
+    $("#learnOne").on("click", function (event) {
+        isExpandedAbt = $("#learnOne").attr("aria-expanded");
+        updateClass();
+    })
+    // On click of contact me button
+    $("#learnTwo").on("click", function (event) {
+        isExpandedContact = $("#learnTwo").attr("aria-expanded");
+        updateClass();
+    })
+    // Function that updates classes for collapsed elements
+    function updateClass() {
+        console.log(`About : ${isExpandedAbt}`);
+        console.log(`Contact : ${isExpandedContact}`);
+
+        if((isExpandedContact === "false") && (isExpandedAbt === "false")) {
+            console.log("Hello");
+            $(".col1").addClass("col");
+            $(".col2").addClass("col");
+        }
+        else if((isExpandedAbt === "false") && ((isExpandedContact === "true") || isExpandedContact === undefined)) {
+            $(".col1").addClass("col");
+            $(".col2").removeClass("col");
+        }
+        else if((isExpandedContact === "false") && ((isExpandedAbt === "true") || isExpandedAbt === undefined)) {
+            $(".col2").addClass("col");
+            $(".col1").removeClass("col");
+        }
+    }
+})
